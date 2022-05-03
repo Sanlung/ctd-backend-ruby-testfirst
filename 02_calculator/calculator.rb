22b42532cdd0dacfd48c48e7a972b5c6e6f8a44f
Chung Kao <chungckao@gmail.com>
2022-05-03 18:03:47 +0800

    Complete lesson 2-3 assignment

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
 #write your code here
+def add(n1, n2)
+  n1 + n2
+end
+
+def subtract(n1, n2)
+  n1 - n2
+end
+
+def sum(arr)
+  arr.reduce(0) { |memo, opr|
+    memo + opr
+  }
+end
+
+def multiply(*args)
+  args.to_a.reduce(1) { |memo, opr|
+    memo * opr
+  }
+end
+
+def power(n1, n2)
+  n1 ** n2
+end
+
+def factorial(n)
+  return 1 if n < 2
+  n * factorial(n - 1)
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def initialize
+    @title = ''
+  end
+
+  def title=(title)
+    noncap_words = ['a', 'an', 'the', 'and', 'or', 'but', 'of', 'for', 'to', 'from', 'in', 'on', 'at', 'by', 'off', 'with', 'over', 'into', 'onto', 'near']
+    cap_title = title.split(' ').map { |word|
+      noncap_words.include?(word) ? word : word.capitalize
+    }.join(' ')
+    @title = cap_title.chr.upcase + cap_title[1..]
+  end
 end
